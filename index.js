cf9f0a98d89f640ef6990c2ef1705b08b85fa191
a13821190779 <945039036@qq.com>
2018-01-11 19:04:00 +0800

    Update: 大概配置了下 vscode

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const Koa = require('koa');
-const app = new Koa();
-const debug = require('debug')('fox');
-const error = require('debug')('fox_Error');
-const log = require('debug')('fox_Info');
+const Koa = require('koa')
+const app = new Koa()
+const debug = require('debug')('fox')
+const error = require('debug')('fox_Error')
+const log = require('debug')('fox_Info')
 
 app.use(async ctx => {
-	ctx.body = '你好';
-});
+    ctx.body = '你好'
+})
 
 app.listen(3000, () => {
-	debug('listening');
-});
+    debug('listening')
+})
 
 app.on('error', err => {
-	error('错误信息:', err);
-});
+    error('错误信息:', err)
+})
