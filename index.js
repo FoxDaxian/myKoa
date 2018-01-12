9cc2b718d5e9be3b4209d4c43e1d81792eaeffe0
a13821190779 <945039036@qq.com>
2018-01-12 14:43:33 +0800

    Update: 增加debug+错误处理

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const Koa = require('koa')
 const app = new Koa()
+const fs = require('fs')
+const path = require('path')
+
+// debug包部分
 const debug = require('debug')('fox')
 const error = require('debug')('fox_Error')
 const log = require('debug')('fox_Info')
+const base_log = require('debug')('请求基本信息')
 
-app.use(async ctx => {
-    ctx.body = '你好'
-})
+const logger = (ctx, next) => {
+    base_log(ctx.method)
+    base_log(ctx.url)
+    next()
+}
 
-app.listen(3000, () => {
-    debug('listening')
+const handler = async (ctx, next) => {
+    try {
+        await next()
+    } catch (err) {
+        ctx.response.status = err.statusCode || err.status || 500
+        ctx.response.body = err
+        error('出错啦', err.message)
+    }
+    
+}
+app.use(logger)
+app.use(handler)
+
+app.use((ctx, next) => {
+    ctx.response.body = '123'
 })
 
 app.on('error', err => {
-    error('错误信息:', err)
+    error('启动失败啦:', err)
+})
+
+app.listen(3000, () => {
+    debug('listening in localhost:3000')
 })
